1bd933ccd929702c192b40ad321b5d411daec5c3
CoverGhost1 <rifqigozali90@gmail.com>
2026-03-01 20:06:51 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Biginteger
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
