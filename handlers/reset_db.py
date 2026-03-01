bbf6993ac96f36a73869ca7d8ba50f69b5803174
CoverGhost1 <rifqigozali90@gmail.com>
2026-03-01 20:13:53 +0700

    Delete handlers/reset_db.py

--- handlers/reset_db.py
+++
@@ -1,9 +0,0 @@
-# reset_db.py
-from database import engine, Base
-from sqlalchemy import inspect
-
-print("Menghapus tabel lama...")
-Base.metadata.drop_all(bind=engine)
-print("Membuat ulang tabel dengan struktur baru...")
-Base.metadata.create_all(bind=engine)
-print("Selesai! Database sudah diperbarui.")
